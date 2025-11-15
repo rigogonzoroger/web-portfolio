ef4fe4947d5a43e66a97e5e43004ed3348d98b51
rigogonzoroger <rigogonzo1@gmail.com>
2025-11-14 22:13:07 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("submit-btn").addEventListener("click", function(event) {
+    event.preventDefault(); // prevents the page from refreshing
+    alert("Thank you for your message!");
+});
